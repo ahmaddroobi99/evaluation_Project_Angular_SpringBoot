2e153a6d1bd8056c820dd4b24e183db7caf2a3f2
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-08-30 10:59:07 +0300

    init

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+import { StringMapWithRename } from "@angular/compiler/src/compiler_facade_interface";
+
+export interface Employee {
+id :number ;
+name :string ; 
+email:string; 
+jobTitle:String;
+phone :string ; 
+imageUrl:string ; 
+employeeCode :string; 
+ 
+}
